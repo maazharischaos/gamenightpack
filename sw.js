d4194c4a099fbee9ab325630d11dfc2ad490038a
maazharischaos <maaz_haris@hotmail.com>
2026-09-02 00:19:46 +0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'gamenight-v12'; // <--- Incremented from v1 to force cache purge
+const CACHE_NAME = 'gamenight-v13'; // <--- Incremented from v1 to force cache purge
 
 const ASSETS_TO_CACHE = [
   './',
