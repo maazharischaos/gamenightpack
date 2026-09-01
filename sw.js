054c6e07042dafe718c412ed98e9cdbc7c1df15b
maazharischaos <maaz_haris@hotmail.com>
2026-09-01 12:29:17 +0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,6 @@
-const CACHE_NAME = 'gamenight-v1';
+// UPDATE THIS VERSION NUMBER WHENEVER YOU DEPLOY NEW CODE
+const CACHE_NAME = 'gamenight-v2';
+
 const ASSETS = [
   './',
   './index.html',
@@ -11,6 +13,7 @@ const ASSETS = [
   './icon-512.png'
 ];
 
+// INSTALL: Force new Service Worker to activate immediately
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
@@ -18,12 +21,35 @@ self.addEventListener('install', (e) => {
   self.skipWaiting();
 });
 
+// ACTIVATE: Delete ALL old caches immediately
 self.addEventListener('activate', (e) => {
-  e.waitUntil(clients.claim());
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    }).then(() => clients.claim())
+  );
 });
 
+// FETCH: Network-First strategy for HTML so fresh code is always loaded if online
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
-  );
+  if (e.request.mode === 'navigate' || e.request.destination === 'document') {
+    e.respondWith(
+      fetch(e.request).then((networkResponse) => {
+        return caches.open(CACHE_NAME).then((cache) => {
+          cache.put(e.request, networkResponse.clone());
+          return networkResponse;
+        });
+      }).catch(() => caches.match(e.request))
+    );
+  } else {
+    e.respondWith(
+      caches.match(e.request).then((res) => res || fetch(e.request))
+    );
+  }
 });
